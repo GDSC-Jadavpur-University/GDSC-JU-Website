7a219af36688c47fe085bc48a6c3daaf0052d8b9
sau-mili <146661411+sau-mili@users.noreply.github.com>
2023-10-02 17:36:03 +0530

    Update hello.js
    API routes edited. Added API route which returns a JSON response with a status code of 200

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  res.status(200).json({ message: 'Welcome to GDSC Jadavpur University' })
 }
