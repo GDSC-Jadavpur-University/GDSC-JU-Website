f6efe6d51444857b2d6f605c06c06491b6006f27
Vidit Agrawal <viditagrawal56@gmail.com>
2023-10-03 19:53:03 +0530

    Applied the suggested changes to the Button and made the button responsive

--- components/BackToTop.jsx
+++ components/BackToTop.jsx
@@ -31,7 +31,7 @@ const BackToTop = () => {
       >
         <button
           onClick={scrollToTop}
-          className="h-16 w-16 bg-slate-500 hover:bg-red-400 hover:-translate-y-2 transition-all ease-in delay-75 rounded-full p-4"
+          className="h-10 w-10 lg:h-16 lg:w-16 bg-black hover:bg-slate-500 hover:-translate-y-2 transition-all ease-in delay-75 rounded-xl p-3 lg:p-4 shadow-md shadow-[rgba(0,0,0,0.35)]"
         >
           <svg
             className="h-full w-full"
@@ -39,6 +39,8 @@ const BackToTop = () => {
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 32 32"
             fill="#fff"
+            stroke="white"
+            strokeWidth={1}
           >
             <path d="m26.71 10.29-10-10a1 1 0 0 0-1.41 0l-10 10 1.41 1.41L15 3.41V32h2V3.41l8.29 8.29z" />
           </svg>
