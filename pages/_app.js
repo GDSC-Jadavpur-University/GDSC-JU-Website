e0673af1f7e875793359fc785dbe6e00056ad73a
Ayush Pandit <ayushpanditmoto@gmail.com>
2023-10-03 14:20:50 +0530

    feat: theme now stored in local storage

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,34 @@
+"use client";
 import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-
 import Layout from "@/layout/Layout";
 import "@/styles/globals.css";
 import { Quicksand } from "next/font/google";
+import { useEffect, useState } from "react";
 
 const quicksand = Quicksand({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
+  //use local storage to store theme
+  const [theme, setTheme] = useState("light");
+  useEffect(() => {
+    const localTheme = localStorage.getItem("theme");
+    if (localTheme) {
+      setTheme(localTheme);
+    }
+  }, []);
+
   return (
     <>
-      <NextUIProvider>
-        <NextThemesProvider attribute="class" defaultTheme="dark">
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </NextThemesProvider>
-      </NextUIProvider>
+      <div className={quicksand.className}>
+        <NextUIProvider>
+          <NextThemesProvider attribute="class" defaultTheme={theme}>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </NextThemesProvider>
+        </NextUIProvider>
+      </div>
     </>
   );
 }
