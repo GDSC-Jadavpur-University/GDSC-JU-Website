3d5d3a7b52fcfee14e3efcf78a2c82a3a0c04eb4
AyushRai <raiayush572@gmail.com>
2023-10-04 16:28:03 +0530

    Events Image Fixed

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,39 +1,42 @@
 import React from "react";
 import Image from "next/image";
 
-function EventCard(props) {
-  return (
-    <a href={props.link}>
-      <div
-        style={{
-          flex: "1",
-          maxWidth: "300px",
-          marginRight: "0.5em",
-          marginBottom: "0.5em",
-          border: "1px solid #ccc",
-          borderRadius: "5px",
-          boxShadow: "0 0 5px rgba(0, 0, 0, 0.3)",
-          display: "flex",
-          alignItems: "center",
-        }}
-      >
-        <Image
-          src={props.image}
-          alt={props.title}
-          width={150}
-          height={150}
-          className="max-w-xs transition duration-300 ease-in-out hover:scale-110"
-        />
-        <div style={{ padding: "1em" }}>
-          <h3 style={{ fontWeight: "bold" }}>{props.title}</h3>
-          <p>{props.description}</p>
-        </div>
-      </div>
-    </a>
-  );
-}
 
 function EventPage() {
+
+  function EventCard(props) {
+    return (
+      <a href={props.link} style={{ textDecoration: "none" }}>
+        <div
+          style={{
+            display: "flex",
+            width: "400px",
+            height: "300px", // Set a fixed height for the card
+            margin: "0.5em",
+            border: "1px solid #ccc",
+            borderRadius: "5px",
+            boxShadow: "0 0 5px rgba(0, 0, 0, 0.3)",
+          }}
+        >
+          <div style={{ flex: "1.5", position: "relative", width: "50%", height: "100%" }}>
+            <Image
+              src={props.image}
+              alt={props.title}
+              layout="fill" // Fill the entire parent container
+              style={{ objectFit: "cover" }} // Set the object fit style
+            />
+          </div>
+          <div style={{ flex: "1", display: "flex", flexDirection: "column", justifyContent: "space-between", padding: "1em", textAlign: "left" }}>
+            <div>
+              <h3 style={{ fontWeight: "bold", marginBottom: "0.5em" }}>{props.title}</h3>
+              <p>{props.description}</p>
+            </div>
+          </div>
+        </div>
+      </a>
+    );
+  }
+    
   return (
     <div style={{ textAlign: "center" }}>
       <strong style={{ fontSize: "2em", fontWeight: "normal" }}>Events</strong>
@@ -56,7 +59,7 @@ function EventPage() {
         />
         <EventCard
           link="https://gdsc.community.dev/events/details/developer-student-clubs-jadavpur-university-kolkata-presents-gdsc-ju-orientation-session/"
-          image="/GDSC Orientation Event.jpg"
+          image="/Orientation Event.jpg"
           title="Orientation Session"
           description="GDSC JU Orientation Session"
         />
